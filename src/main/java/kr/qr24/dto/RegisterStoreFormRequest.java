f2119b81aae368ce7d9b2b2fd0be4f7fee074d7b
ravi <artiveloper@gmail.com>
2020-05-27 13:14:57 +0900

    fix: 매장 정보 등록 시 유효성 체크를 추가한다

--- src/main/java/kr/qr24/dto/RegisterStoreFormRequest.java
+++ src/main/java/kr/qr24/dto/RegisterStoreFormRequest.java
@@ -3,25 +3,34 @@ package kr.qr24.dto;
 import kr.qr24.domain.Store;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class RegisterStoreFormRequest {
 
+    @NotBlank(message = "매장 정보를 입력해주세요")
     private String name;
 
+    @NotBlank(message = "우편 번호를 입력해주세요")
     private String postcode;
 
+    @NotBlank(message = "도로명 주소를 입력해주세요")
     private String roadAddress;
 
+    @NotBlank(message = "지번 주소를 입력해주세요")
     private String jibunAddress;
 
     private String detailAddress;
 
     private String extraAddress;
 
+    @NotBlank(message = "사업자번호를 입력해주세요")
     private String businessNumber;
 
+    @NotBlank(message = "담당자 성함을 입력해주세요")
     private String managerName;
 
+    @NotBlank(message = "담당자 연락처를 입력해주세요")
     private String managerPhoneNumber;
 
     public Store toStoreEntity() {
