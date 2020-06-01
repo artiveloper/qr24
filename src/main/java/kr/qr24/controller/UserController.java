b51774382cad1cc12c59add36490bc2819aaae5a
ravi <artiveloper@gmail.com>
2020-06-01 16:05:49 +0900

    fix: 회원 가입시 로그인 화면에서 가입한 메일 주소를 보여주도록 수정한다

--- src/main/java/kr/qr24/controller/UserController.java
+++ src/main/java/kr/qr24/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -40,10 +41,11 @@ public class UserController {
     }
 
     @PostMapping("/sign-up")
-    public String doSignUp(@Valid SignUpFormRequest signUpFormRequest, Errors errors) {
+    public String doSignUp(@Valid SignUpFormRequest signUpFormRequest, Errors errors, RedirectAttributes redirectAttributes) {
         if (errors.hasErrors()) {
             return "users/sign-up";
         }
+        redirectAttributes.addFlashAttribute("welcomeEmail", signUpFormRequest.getEmail());
         userService.signUp(signUpFormRequest);
         return "redirect:/users/sign-in";
     }
